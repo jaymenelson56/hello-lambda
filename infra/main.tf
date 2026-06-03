7cb22bfcbc581745421435b781dd1f88b45eb251
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2026-06-03 15:14:44 -0500

    first push

--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,11 @@
 terraform {
-  backend "s3" {
-    bucket = "jayme-hello-lambda-tfstate"
-    key    = "hello-lambda/terraform.tfstate"
-    region = "us-east-2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
-
 provider "aws" {
   region = var.aws_region
 }
